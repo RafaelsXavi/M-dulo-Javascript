c48e44166fd22f61c3f9bb4d5ff8536534b6913f
RafaelsXavi <rafaelx277@gmail.com>
2025-09-15 18:24:42 -0300

    Anotacoes

---
+++ JSnapratica/scripts.js
@@ -0,0 +1,14 @@
+
+
+
+
+const input = document.querySelector("#main-input");
+
+function clickButton() {
+    console.log(input.value);
+}
+
+
+function digiteinoinput() {
+    console.log("Digitei no input");
+}
