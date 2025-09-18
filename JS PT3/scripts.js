2091a7236882607f81d890f8098c4e4fc4d35030
RafaelsXavi <rafaelx277@gmail.com>
2025-09-18 00:49:13 -0300

    JS PT3

---
+++ JS PT3/scripts.js
@@ -0,0 +1,16 @@
+/*  
+math -> biblioteca nativa do JS que possui diversas funcionalidades matemáticas
+random() -> função que gera um número aleatório entre 0 e 1
+floor() -> função que arredonda um número para baixo
+ceil() -> função que arredonda um número para cima
+round() -> função que arredonda um número para o inteiro mais próximo
+pow() -> função que eleva um número a uma potência
+sqrt() -> função que calcula a raiz quadrada de um número
+abs() -> função que retorna o valor absoluto de um número
+max() -> função que retorna o maior valor entre os argumentos fornecidos
+min() -> função que retorna o menor valor entre os argumentos fornecidos
+
+
+
+
+*/
--- Projeto na pratica/scripts.js
+++ Projeto na pratica/scripts.js
@@ -4,11 +4,11 @@
 const convertbutton = document.getElementById("convert-button")
 const currencySelect = document.getElementById("currency-select")
 
-function convertValues (){
+function convertValues() {
     const inputvalue = document.querySelector(".input-value").value
     const currencyToConvert = document.querySelectorAll("select")[1].value
-    
-    console.log(currencySelect)    
+
+    console.log(currencySelect)
     console.log("convertValues")
     console.log("Valor digitado:", inputvalue)
     console.log("Moeda para converter:", currencyToConvert)
@@ -16,13 +16,26 @@ function convertValues (){
     // Cotações fixas
     const dolarToday = 5.25
     const euroToday = 6.31
+    const realToday = 1
+    const bitcoinToday = 134.000
+
     let convertedValue = 0
     let currencyCode = ""
 
-    if(currencyToConvert === "dolar"){
+
+    if (currencyToConvert === "bitcoin") {
+        convertedValue = inputvalue / bitcoinToday
+        currencyCode = "btc"
+    }
+    if (currencyToConvert === "real") {
+        convertedValue = inputvalue / realToday
+        currencyCode = "BRL"
+    }
+    if (currencyToConvert === "dolar") {
         convertedValue = inputvalue / dolarToday
         currencyCode = "USD"
-    } else if(currencyToConvert === "euro"){
+    }
+    if (currencyToConvert === "euro") {
         convertedValue = inputvalue / euroToday
         currencyCode = "EUR"
     }
@@ -37,12 +50,22 @@ function convertValues (){
     dolarValueText.textContent = new Intl.NumberFormat('en-US', { style: 'currency', currency: currencyCode }).format(convertedValue)
 
     // Atualizar a imagem da moeda e texto
-
-    if(currencyToConvert === "dolar"){
+    if (currencyToConvert === "real") {
+        currencyImage.src = "./brasil.png"
+        currencyImage.alt = "real"
+        currencyText.textContent = "real"
+    }
+    if (currencyToConvert === "bitcoin") {
+        currencyImage.src = "./assets/bitcoin.png"
+        currencyImage.alt = "bitcoin"
+        currencyText.textContent = "bitcoin"
+    }
+    if (currencyToConvert === "dolar") {
         currencyImage.src = "./assets/estados-unidos (1) 1.png"
-        currencyImage.alt = "Dólar"
-        currencyText.textContent = "Dólar"
-    } else if(currencyToConvert === "euro"){
+        currencyImage.alt = "Dolar"
+        currencyText.textContent = "Dolar"
+    }
+    if (currencyToConvert === "euro") {
         currencyImage.src = "./assets/icons8-euro-48.png"
         currencyImage.alt = "Euro"
         currencyText.textContent = "Euro"
@@ -50,7 +73,7 @@ function convertValues (){
 
     // Chamar a função de conversão ao mudar a seleção da moeda
 
-    
+
 
 }
 
@@ -58,5 +81,4 @@ function convertValues (){
 
 convertbutton.addEventListener("click", convertValues)
 
-// Adicionar listener para mudança na seleção da moeda
 document.querySelectorAll("select")[1].addEventListener("change", convertValues)
